6555c08356a1929513bdc4beab61df001add75ae
1F10230014 <s1f102300144@iniad.org>
2024-06-28 23:06:04 +0900

    add:controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, ValidationPipe } from '@nestjs/common';
+import { User } from './user';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+  @Post()
+  create(@Body(ValidationPipe) user: User) {
+    return this.usersService.create(user);
+  }
+}
