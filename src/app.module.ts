0db415832f1a08763e8f7eb7336c11b760b600b6
1F10230014 <s1f102300144@iniad.org>
2024-06-30 05:08:48 +0900

    change: ディクトリ構成の変更

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 
-@Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
-})
+@Module({})
 export class AppModule {}
